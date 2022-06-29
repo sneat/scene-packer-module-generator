5114e25d3c2a2248dcce5c8e2ec3b31afe1ddd5d
Blair McMillan <blair@mcmillan.id.au>
2022-06-29 15:05:53 +1000

    chore(deps-dev): bump eslint-plugin-vue from 8.7.1 to 9.1.1

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,7 +19,7 @@
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -30,7 +30,7 @@
       app
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title v-text="title" />
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
     </v-app-bar>
     <v-main>
       <v-container>
